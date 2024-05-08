c48f5e6b40de489ff7471adea917022d4db2aed8
Fabhay <72580053+fabhay2@users.noreply.github.com>
2024-05-08 18:31:30 -0400

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -547,7 +547,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://gist.githubusercontent.com/fabhay2/4c39aec14fbbad6e1f1eadce4b3cd721/raw/fefef46288adc2bbc4f7d6c6394d15428d1cea97/distribution.json'
+        const distroURL = 'https://gist.githubusercontent.com/fabhay2/4c39aec14fbbad6e1f1eadce4b3cd721/raw/d7bb6df913251811fbd9e4b0ba0de35ecfef647c/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
