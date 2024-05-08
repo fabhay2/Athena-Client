a23d0e820404442471454941b437610fd283004c
Fabhay <72580053+fabhay2@users.noreply.github.com>
2024-05-07 20:09:43 -0400

    Test Version

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -547,7 +547,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+        const distroURL = 'https://gist.githubusercontent.com/fabhay2/4c39aec14fbbad6e1f1eadce4b3cd721/raw/15d9c35dc625e3fe247337e7d456d51d742edfd0/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
